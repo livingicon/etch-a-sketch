e85c98fa5ed43d4bd3cad46532e5030bd0f8cab8
Josh Hancock <livingicon@gmail.com>
2022-02-18 17:31:03 -0500

    Add clear button to reload page

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const sketchPad = document.querySelector('#container');
+const clear = document.querySelector('#btnClear');
 
-function notePad(gridSize) {
+clear.addEventListener('click', reload, false);
+
+function notePad(gridSize = 16) {
   for (let i = 0; i < (gridSize * gridSize); i++) {
     const measurement = (960 / gridSize);
+
     const square = document.createElement('div');
-    square.addEventListener('mouseover', mouseOver);
+
+    square.addEventListener('mouseenter', draw);
 
     square.classList.add('square');
     square.style.width = `${measurement}px`;
     square.style.heigth = `${measurement}px`;
-    square.style.flex
 
     sketchPad.appendChild(square);
   }
 };
 
-notePad(25);
+notePad();
 
-function mouseOver(e) {
-  e.target.style.backgroundColor = "red";
+function draw(e) {
+  e.target.style.backgroundColor = "black";
 }
+
+function reload() {
+  reload = location.reload();
+}
+
+//set limit to 100
