2dd0408f0e76046a936ae3bf0e83f3dc56242217
Josh Hancock <livingicon@gmail.com>
2022-02-18 14:23:26 -0500

    Add temp hover and make grid size dynamic

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const sketchPad = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement('div');
-  grid.classList.add('square');
-  grid.style.flex
-  sketchPad.appendChild(grid);
+function notePad(gridSize) {
+  for (let i = 0; i < (gridSize * gridSize); i++) {
+    const measurement = (960 / gridSize);
+    const grid = document.createElement('div');
+    grid.classList.add('square');
+    grid.style.width = `${measurement}px`;
+    grid.style.heigth = `${measurement}px`;
+    grid.style.flex
+
+    sketchPad.appendChild(grid);
+  }
+};
+
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
 }
+
+notePad(16);
