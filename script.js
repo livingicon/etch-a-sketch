0a633822c0257b92ed7a56ac7ae6d92ef9a361a0
Josh Hancock <livingicon@gmail.com>
2022-02-18 15:18:52 -0500

    Add mousedown event to change background color

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const sketchPad = document.querySelector('#container');
 function notePad(gridSize) {
   for (let i = 0; i < (gridSize * gridSize); i++) {
     const measurement = (960 / gridSize);
-    const grid = document.createElement('div');
-    grid.classList.add('square');
-    grid.style.width = `${measurement}px`;
-    grid.style.heigth = `${measurement}px`;
-    grid.style.flex
+    const square = document.createElement('div');
+    square.addEventListener('mouseover', mouseOver);
 
-    sketchPad.appendChild(grid);
+    square.classList.add('square');
+    square.style.width = `${measurement}px`;
+    square.style.heigth = `${measurement}px`;
+    square.style.flex
+
+    sketchPad.appendChild(square);
   }
 };
 
-function mouseOver() {
-  document.getElementById("demo").style.color = "red";
-}
+notePad(25);
 
-notePad(16);
+function mouseOver(e) {
+  e.target.style.backgroundColor = "red";
+}
