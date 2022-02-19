49ecd38697ebbad279dd4be897e53dae983140f4
Josh Hancock <livingicon@gmail.com>
2022-02-18 21:54:36 -0500

    Change button function to remove/replace drawing grid

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const sketchPad = document.querySelector('#container');
+const sketch = document.querySelector('#sketch');
 const clear = document.querySelector('#btnClear');
 
-clear.addEventListener('click', reload, false);
+clear.addEventListener('click', reload);
+
+notePad();
 
 function notePad(gridSize = 16) {
+  const sketchPad = document.createElement('div');
+  sketchPad.classList.add('container');
+  sketch.appendChild(sketchPad);
+
   for (let i = 0; i < (gridSize * gridSize); i++) {
     const measurement = (960 / gridSize);
 
@@ -13,20 +19,28 @@ function notePad(gridSize = 16) {
 
     square.classList.add('square');
     square.style.width = `${measurement}px`;
-    square.style.heigth = `${measurement}px`;
+    square.style.height = `${measurement}px`;
 
     sketchPad.appendChild(square);
   }
-};
-
-notePad();
+}
 
 function draw(e) {
   e.target.style.backgroundColor = "black";
 }
 
 function reload() {
-  reload = location.reload();
+  const gridSize = prompt("How many?", 16);
+  if (gridSize != null) {//not working to keep it from deleting if there is null value
+    const refresh = document.querySelector('.container');
+    sketch.removeChild(refresh);
+    notePad(gridSize);
+  }
 }
 
-//set limit to 100
+/*
+const gridSize = prompt("How many?", 16);
+if (gridSize != null) {
+  notePad(gridSize);
+}
+*/
