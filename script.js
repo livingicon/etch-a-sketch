f2ede119fba53d04396e53a43d928f2ca8139198
Josh Hancock <livingicon@gmail.com>
2022-02-20 15:30:23 -0500

    Final styling. Delete extra JS to focus on required content.

--- script.js
+++ script.js
@@ -12,17 +12,13 @@ function notePad(gridSize = 16) {
 
   for (let i = 0; i < (gridSize * gridSize); i++) {
     const measurement = (960 / gridSize);
-
     const square = document.createElement('div');
-
-    square.addEventListener('mouseenter', draw);
-
     square.classList.add('square');
     square.style.width = `${measurement}px`;
     square.style.height = `${measurement}px`;
-
     sketchPad.appendChild(square);
-  }
+    square.addEventListener('mouseenter', draw);
+  } 
 }
 
 function draw(e) {
@@ -30,34 +26,21 @@ function draw(e) {
 }
 
 function reset() {
-  const gridSize = prompt("Input desired grid size number between 16-100", 16);
-  if(gridSize < 16) {//this covers cancel
+  const gridSize = prompt("Input desired canvas size number between 16-100", 16);
+  if (isNaN(Number(gridSize)) === true) {
+    alert("That won't work. You must enter a number.");
+    reset();
+  } else if (gridSize < 16) {
     const refresh = document.querySelector('.container');
     sketch.removeChild(refresh);
     notePad();
-    console.log(gridSize);
-    console.log(typeof(gridSize));
   } else if(gridSize > 100) {
     const refresh = document.querySelector('.container');
     sketch.removeChild(refresh);
     notePad(100);
-    console.log(Number(gridSize));
-    console.log(typeof(gridSize));
-  }/* else if (Number(gridSize) == NaN) {//this is keeping it from disappearing
-    const refresh = document.querySelector('.container');
-    sketch.removeChild(refresh);
-    notePad(16);
-    console.log(gridSize);
-    console.log(typeof(gridSize));
-  }*/ else {
+  } else {
     const refresh = document.querySelector('.container');
     sketch.removeChild(refresh);
     notePad(gridSize);
-    console.log(gridSize);
-    console.log(typeof(gridSize));
-    console.log(Number(gridSize));
   }
 }
-
-//not accept over 100 and must accept a number
-//make it mousedown and mouseenter?
